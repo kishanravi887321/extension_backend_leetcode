ba1d914cd800ad463ec98e629ad1eabab9cea17c
Ravi Kishan <kishanravi887321@gmail.com>
2026-01-10 22:02:42 +0530

    feat: add user routes for registration and login

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,7 @@
+import { loginUser } from "../controllers/user.controllers.js";
+import { registerUser } from "../controllers/user.controllers.js";
+import express from "express";
+const router = express.Router();
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+export default router;
