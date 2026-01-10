1afaa3cd70814e9c2aef36d62b3b516ed72b2517
Ravi Kishan <kishanravi887321@gmail.com>
2026-01-10 21:45:53 +0530

    feat: refactor server startup logic by encapsulating it in a startServer function

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,11 @@
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log(`Server running on http://localhost:3000`);
-});
+
+
+const startServer =()=>{
+    app.listen(3000, () => {
+      console.log(`Server running on http://localhost:3000`);
+    });
+}
+
+startServer();
