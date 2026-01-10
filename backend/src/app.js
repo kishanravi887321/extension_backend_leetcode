42ef48d85a1dd132a3ff4200c16ef7b83de28ac9
Ravi Kishan <kishanravi887321@gmail.com>
2026-01-10 23:03:42 +0530

    feat: update Auth styles for improved aesthetics and user experience

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,9 @@ import {router as userRoutes} from "./routes/user.routes.js";
 const app = express();
 app.use(express.json());
 app.use("/api/users", userRoutes);
+app.get("/",(req,res)=>{
+    res.send("server  is running...");
+});
 
 const PORT = 3000;
 export default app;
