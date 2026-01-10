94adac24bc7437626502e5d02781bd801dadd1d4
Ravi Kishan <kishanravi887321@gmail.com>
2026-01-10 21:59:10 +0530

    feat: rename QustSchema to UserSchema and implement password hashing and matching methods

--- backend/src/models/User.models.js
+++ backend/src/models/User.models.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const QustSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
  
     username:{
         type:String,
@@ -24,4 +24,18 @@ const QustSchema = new mongoose.Schema({
   
 });
 
-const Qust = mongoose.model("Qust", QustSchema);
+UserSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+UserSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+
+const User = mongoose.model("User", UserSchema);
