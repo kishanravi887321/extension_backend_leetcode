bbf66fa8237084f0ba74467a4fa1d47363baf949
Ravi Kishan <kishanravi887321@gmail.com>
2026-01-11 16:48:06 +0530

    Add cp.saksin.online to CORS whitelist

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,14 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://cpcoders.saksin.online', 'http://cpcoders.saksin.online'],
+  origin: [
+    'http://localhost:5173', 
+    'http://localhost:3000', 
+    'https://cpcoders.saksin.online', 
+    'http://cpcoders.saksin.online',
+    'https://cp.saksin.online',
+    'http://cp.saksin.online'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
