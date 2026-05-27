c49fb4a777bb8754ff795a43b948fc1a8d5d520e
Ravi Kishan <kishanravi887321@gmail.com>
2026-05-27 21:30:56 +0530

    feat: update extension guide button text and add title for accessibility

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -181,9 +181,10 @@ const Login = () => {
             whileHover={{ scale: 1.02, y: -2 }}
             whileTap={{ scale: 0.98 }}
             onClick={() => setShowHowToUse(true)}
+            title="View a quick extension setup guide"
           >
             <span className="wave-htu-icon">📖</span>
-            <span className="wave-htu-text">How to Use Extension</span>
+            <span className="wave-htu-text">Open Extension Guide</span>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="wave-htu-arrow">
               <path fillRule="evenodd" d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z" clipRule="evenodd" />
             </svg>
